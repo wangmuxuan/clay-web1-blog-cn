cc6f99f418d6e5502a3a3e19fadb13b311c4052c
wangmuxuan <chenxiaohui2007@gmail.com>
2026-05-12 13:28:22 +0800

    Add config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    'site_name' => "CLAY 的个人博客",
+    'tagline' => '一个运行在信息高速公路上的复古个人网站。',
+    'base_url' => '',
+    'email' => 'clay@internetmail.example',
+    'admin_password' => 'changeme',
+    'posts_per_page' => 5,
+    'visitor_cookie_days' => 30,
+    'blogroll' => array(
+        array('name' => '复古门户在线', 'url' => 'https://example.com'),
+        array('name' => '银河社区 1999', 'url' => 'https://example.com'),
+        array('name' => '软盘城市', 'url' => 'https://example.com'),
+        array('name' => '老式企业导航站', 'url' => 'https://example.com'),
+        array('name' => '我最喜欢的搜索页', 'url' => 'https://example.com')
+    )
+);
